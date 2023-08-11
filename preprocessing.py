9d9df61d1a014b762ccad1dd9ad7a371927ead25
alexKumarDoT <alexander.kumar.ctr@dot.gov>
2023-08-11 15:23:28 -0400

    first commit

---
+++ preprocessing.py
@@ -0,0 +1,6 @@
+# read in main csv file
+
+# make dictionary: key = node id, value = node properties
+# use dictionary to create ties
+
+# export into multiple csvs organized under main dir -> labels, relationships -> ... csvs ...
